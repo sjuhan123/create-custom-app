25dfd85c971d2e4796cff6ca39ee1dfac128029b
sjuhan123 <sjuhan123@gmail.com>
2023-08-19 20:59:12 +0900

    🐛 fix: working directory가 .일 때 종료되는 현상 수정

--- bin/create-customized-app.js
+++ bin/create-customized-app.js
@@ -22,15 +22,17 @@ program
     const projectName = folderName;
     const projectPath = path.join(process.cwd(), projectName);
 
-    if (!fs.existsSync(projectPath)) {
-      fs.mkdirSync(projectPath);
-    } else {
-      console.log(
-        `The folder ${chalk.red(
-          folderName
-        )} already exist in the current directory, please give it another name.`
-      );
-      process.exit(1);
+    if (projectName !== ".") {
+      if (!fs.existsSync(projectPath)) {
+        fs.mkdirSync(projectPath);
+      } else {
+        console.log(
+          `The folder ${chalk.red(
+            folderName
+          )} already exist in the current directory, please give it another name.`
+        );
+        process.exit(1);
+      }
     }
 
     process.chdir(projectPath);
