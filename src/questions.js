8b7661f248c91c6cb441318b4812c6074bd98b5f
sjuhan123 <sjuhan123@gmail.com>
2023-09-05 15:13:36 +0900

    ♻️ refactor: questions choices value 형태 일치화

--- src/questions.js
+++ src/questions.js
@@ -3,25 +3,37 @@ const questions = [
     type: "list",
     name: "reactEnvironment",
     message: "Select the react environment:",
-    choices: ["React + JavaScript", "React + TypeScript"],
+    choices: [
+      { name: "React + JavaScript", value: "React + JavaScript" },
+      { name: "React + TypeScript", value: "React + TypeScript" },
+    ],
   },
   {
     type: "list",
     name: "useReactRouterDom",
     message: "Use React Router Dom?",
-    choices: ["Yes", "No"],
+    choices: [
+      { name: "Yes", value: "Yes" },
+      { name: "No", value: "No" },
+    ],
   },
   {
     type: "list",
     name: "styleLibrary",
     message: "Select a style library:",
-    choices: ["styled-components", "none"],
+    choices: [
+      { name: "styled-components", value: "styled-components" },
+      { name: "None", value: "None" },
+    ],
   },
   {
     type: "list",
     name: "usePrettierEslint",
     message: "Use Prettier and ESLint?",
-    choices: ["Yes", "No"],
+    choices: [
+      { name: "Yes", value: "Yes" },
+      { name: "No", value: "No" },
+    ],
   },
 ];
 
